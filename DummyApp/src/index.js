f913844c0801f8b35e19209c39ce462ebb91e3e3
Vedsted <jonso16@student.sdu.dk>
2019-11-20 11:12:55 +0100

    dummy updated. Run with: npm start

--- DummyApp/src/index.js
+++ DummyApp/src/index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const ps = require('./pathSmoother');
 var cors = require('cors');
 let arguments = process.argv.splice(2);
@@ -9,20 +10,33 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cors());
 
-const port = arguments[0] || 8081;
+
+const port = arguments[0] || 8080;
+
+app.get('/', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.sendFile(path.join(__dirname, 'index.html'))
+});
 
 
 app.post('/median', (req, res, next) => {
+    console.log(req.headers.cookie);
     let data = req.body.data;
     let filterGranularity = req.body.filterGranularity;
 
+    res.cookie('TrackingCookie', 'TrackingCookieValue')
     res.send(ps.median(data, filterGranularity));
 });
 
+
 app.post('/mean', (req, res, next) => {
+    console.log(req.headers.cookie)
     let data = req.body.data;
     let filterGranularity = req.body.filterGranularity;
 
+    res.cookie('TrackingCookie', 'TrackingCookieValue')
     res.send(ps.mean(data, filterGranularity));
 });
 
