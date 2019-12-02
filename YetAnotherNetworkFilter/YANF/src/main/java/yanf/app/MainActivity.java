57cb0c5211ba10481431ba5faca103148c2fbe5b
Christian Clausen <csbc92@hotmail.com>
2019-12-02 14:30:27 +0100

    Alternative UI layout with Switches

--- YetAnotherNetworkFilter/YANF/src/main/java/yanf/app/MainActivity.java
+++ YetAnotherNetworkFilter/YANF/src/main/java/yanf/app/MainActivity.java
@@ -4,34 +4,30 @@ package yanf.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.Switch;
 import android.widget.ToggleButton;
-
 
 import com.github.megatronking.netbare.NetBare;
 import com.github.megatronking.netbare.NetBareConfig;
 import com.github.megatronking.netbare.NetBareListener;
 import com.github.megatronking.netbare.http.HttpInjectInterceptor;
 import com.github.megatronking.netbare.http.HttpInterceptorFactory;
+import com.github.megatronking.netbare.injector.HttpInjector;
 import com.github.megatronking.netbare.ssl.JKS;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements NetBareListener, AdapterView.OnItemSelectedListener {
     private static final String TAG = "MainActivity";
@@ -47,26 +43,21 @@ public class MainActivity extends AppCompatActivity implements NetBareListener, 
     private boolean cookieClicked = false;
 
     private String currentlySelectedBlackList;
+    private Map<String, HttpInjector> injectors = new HashMap<>();
+    private static final String COOKIE_INJECTOR = "COOKIE_INJECTOR";
+    private static final String ADS_INJECTOR = "ADS_INJECTOR";
+    private HttpInjector cookieInjector;
+    private HttpInjector adsInjector;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ((Spinner)findViewById(R.id.spinner)).setOnItemSelectedListener(this);
+        initializeFilterSelection();
+        initializeInterceptors();
+        initializeSwitches();
 
-        /*cookieButton = findViewById(R.id.cookieButton);
-        cookieButton.setOnClickListener(v -> {
-            Log.i("Cookie button ", "is clicked");
-            cookieClicked = true;
-        });
-
-        adsButton = findViewById(R.id.adsButton);
-        adsButton.setOnClickListener(v -> {
-            Log.i("Advertisement button ", "is clicked");
-            adsClicked = true;
-        });
-*/
         mNetBare = NetBare.get();
 
         mActionButton = findViewById(R.id.action);
@@ -74,13 +65,78 @@ public class MainActivity extends AppCompatActivity implements NetBareListener, 
             if (mNetBare.isActive()) {
                 mNetBare.unregisterNetBareListener(this);
                 mNetBare.stop();
-
+                enableUIElements();
             } else {
                 prepareNetBare();
+                disableUIElements();
             }
         });
 
         mNetBare.registerNetBareListener(this);
+    }
+
+    private void disableUIElements() {
+        findViewById(R.id.switchAds).setEnabled(false);
+        findViewById(R.id.switchCookies).setEnabled(false);
+        findViewById(R.id.spinner).setEnabled(false);
+        mActionButton.setText(R.string.stop_yanf);
+    }
+
+    private void enableUIElements() {
+        findViewById(R.id.switchAds).setEnabled(true);
+        findViewById(R.id.switchCookies).setEnabled(true);
+        findViewById(R.id.spinner).setEnabled(true);
+        mActionButton.setText(R.string.start_yanf);
+    }
+
+    private void initializeFilterSelection() {
+        ((Spinner)findViewById(R.id.spinner)).setOnItemSelectedListener(this);
+        currentlySelectedBlackList = "None";
+    }
+
+    private void initializeInterceptors() {
+        this.cookieInjector = new CookieInterceptor(LoadBlackList.loadBlackList(this).getDomains(currentlySelectedBlackList));
+        this.adsInjector = new AdvertisementInjector(LoadBlackList.loadBlackList(this).getDomains(currentlySelectedBlackList));
+    }
+
+    private void initializeSwitches() {
+        Switch switchCookies = findViewById(R.id.switchCookies);
+        Switch switchAds = findViewById(R.id.switchAds);
+        switchCookies.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    if (!injectors.containsKey(COOKIE_INJECTOR)) {
+                        injectors.put(COOKIE_INJECTOR, cookieInjector);
+                    }
+                    // Set the style of the switch
+                    switchCookies.setSwitchTextAppearance(getApplicationContext(), R.style.SwitchTextAppearanceOn);
+                } else {
+                    if (injectors.containsKey(COOKIE_INJECTOR)) {
+                        injectors.remove(COOKIE_INJECTOR);
+                    }
+                    switchCookies.setSwitchTextAppearance(getApplicationContext(), R.style.SwitchTextAppearanceOff);
+                }
+            }
+        });
+
+        switchAds.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    if (!injectors.containsKey(ADS_INJECTOR)) {
+                        injectors.put(ADS_INJECTOR, adsInjector);
+                    }
+                    // Set the style of the switch
+                    switchAds.setSwitchTextAppearance(getApplicationContext(), R.style.SwitchTextAppearanceOn);
+                } else {
+                    if (injectors.containsKey(ADS_INJECTOR)) {
+                        injectors.remove(ADS_INJECTOR);
+                    }
+                    switchAds.setSwitchTextAppearance(getApplicationContext(), R.style.SwitchTextAppearanceOff);
+                }
+            }
+        });
     }
 
     @Override
@@ -124,13 +180,9 @@ public class MainActivity extends AppCompatActivity implements NetBareListener, 
         // Start the NetBare service
         Log.i(TAG, "prepareNetBare: Netbare starting");
 
-        if(adsClicked) {
-            mNetBare.start(NetBareConfig.defaultHttpConfig(App.getInstance().getJSK(), adsInterceptor()));
-        }
+        List<HttpInterceptorFactory> interceptorFactories = getInterceptorFactoriesList(this.injectors.values().toArray(new HttpInjector[0]));
+        mNetBare.start(NetBareConfig.defaultHttpConfig(App.getInstance().getJSK(), interceptorFactories));
 
-        if (cookieClicked) {
-            mNetBare.start(NetBareConfig.defaultHttpConfig(App.getInstance().getJSK(), cookiesInterceptor()));
-        }
     }
 
     @Override
@@ -141,14 +193,14 @@ public class MainActivity extends AppCompatActivity implements NetBareListener, 
         }
     }
 
-    private List<HttpInterceptorFactory> adsInterceptor() {
-        HttpInterceptorFactory adInterceptor = HttpInjectInterceptor.createFactory(new AdvertisementInjector(LoadBlackList.loadBlackList(this).getDomains(currentlySelectedBlackList)));
-        return Arrays.asList(adInterceptor);
-    }
+    private List<HttpInterceptorFactory> getInterceptorFactoriesList(HttpInjector... injectors) {
+        ArrayList<HttpInterceptorFactory> factories = new ArrayList<>();
 
-    private List<HttpInterceptorFactory> cookiesInterceptor() {
-        HttpInterceptorFactory cookieInterceptor = HttpInjectInterceptor.createFactory(new CookieInterceptor(LoadBlackList.loadBlackList(this).getDomains(currentlySelectedBlackList)));
-        return Arrays.asList(cookieInterceptor);
+        for (int i = 0; i < injectors.length; i++) {
+            factories.add(HttpInjectInterceptor.createFactory(injectors[i]));
+        }
+
+        return factories;
     }
 
 
@@ -161,4 +213,4 @@ public class MainActivity extends AppCompatActivity implements NetBareListener, 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) { }
 
-    }
+}
